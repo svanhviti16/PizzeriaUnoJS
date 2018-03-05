e000a7f930eebe890838b4f4c00fc29e715f7c02
Sara Rut <saraf16@ru.is>
2018-03-05 21:59:44 +0000

    reyna gra add to card virkar ekki

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { CartList }  from '../CartList/CartList';
+import { connect } from 'react-redux';
 
-const Cart = () => {
-    return (
+class Cart extends React.Component {
+    render() {
+        console.log(this.props);
+      return (
         <div className="cart">
             <h2>Pay for your pizza please</h2>
+            <CartList
+            cartList={this.props.cart}
+             /> 
         </div>
-    )
+      )
+    }
 };
 
-export default Cart;
+function mapStateToProps(state) {
+    return {
+      cart: state.cart,
+    }
+  }
+  
+  export default connect(mapStateToProps, null)(Cart);
+
+
+/*  import React from 'react';
+import { connect } from 'react-redux';
+import Pizza from '../Pizza/Pizza';
+import { getAllPizzas } from '../../actions/pizzaActions';
+
+class Menu extends React.Component {
+
+    componentDidMount() {
+        const { getAllPizzas } = this.props;
+        getAllPizzas();
+    }
+    render () {
+        const { pizza } = this.props;
+        return (
+            <div className="pizza-container">
+                {pizza.map(p =>  <Pizza key={p.id} pizza={p} />  )}
+            </div>
+        );
+    }
+};
+
+const mapStateToProps = (state) => {
+    
+    return {
+        pizza: state.pizza
+    }
+}
+export default connect(mapStateToProps, { getAllPizzas })(Menu);*/
--- client/src/components/PizzaId/PizzaId.js
+++ client/src/components/PizzaId/PizzaId.js
@@ -3,6 +3,8 @@ import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux';
 import Pizza from '../Pizza/Pizza';
 import { getPizzaById } from '../../actions/pizzaActions';
+import { addToCart } from '../../actions/cartActions';
+
 
 class PizzaId extends React.Component {
     /* constructor(props) {
@@ -14,6 +16,11 @@ class PizzaId extends React.Component {
         console.log('id ' + params.Id);
         getPizzaById(params.Id);
     }
+
+    handleOnClickCart (pizza) {
+        const { addToCart } = this.props;
+        addToCart(pizza);
+    }
     
     render() {
         const { pizzaId } = this.props;
@@ -23,7 +30,12 @@ class PizzaId extends React.Component {
                     <Pizza key={pizzaId} pizza={pizzaId} /> 
                 }
                 </div>
+            <button className="btn btn-primary" onClick={() => {
+                this.handleOnClickCart(pizzaId);
+              }}
+            > Add to Cart</button>
             </div>
+
         );
     }
 }
@@ -35,4 +47,12 @@ const mapStateToProps = (state) => {
         pizzaId: state.pizzaId
     }
 }
-export default connect(mapStateToProps, { getPizzaById })(PizzaId);
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        getPizzaById: getPizzaById,
+        addToCart: addToCart, 
+    }, dispatch)
+  
+  }
+export default connect(mapStateToProps, mapDispatchToProps)(PizzaId);
