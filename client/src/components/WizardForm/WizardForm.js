83ed9758adcd3320d4f207e3f8c765124be164f3
Svanhvit <svanhviti16@ru.is>
2018-03-08 17:41:22 +0000

    laga indent

--- client/src/components/WizardForm/WizardForm.js
+++ client/src/components/WizardForm/WizardForm.js
@@ -77,21 +77,19 @@ class WizardForm extends Component {
                 {page === 1 && 
                     <Grid className="container-margin">
                         <div>
-                            <label>Pickup or Delivery</label>
-                                <TextInput
-                                    onChange={ e => this.handleClickPickup(e) }
-                                    type="radio"
-                                    name="pickup"
-                                    value= "false"
-                                    validate={val => !val ? 'Name is required' : ''} />
-                                />
-                                <TextInput
-                                  onChange={ e => this.handleClickDelivery(e) }
-                                  type="radio"
-                                  name="delivery"
-                                  value= "true"
-                                  validate={val => !val ? 'Name is required' : ''} />
-                                 />
+                            <label>Pickup or Delivery?</label>
+                            <TextInput
+                                onChange={ e => this.handleClickPickup(e) }
+                                type="radio"
+                                name="Pickup"
+                                value= "false"
+                                validate={val => !val ? 'Name is required' : ''} />
+                            <TextInput
+                                onChange={ e => this.handleClickDelivery(e) }
+                                type="radio"
+                                name="Delivery"
+                                value= "true"
+                                validate={val => !val ? 'Name is required' : ''} />
                         </div>
                         <div>
                             <button type="button" className="next" onClick={this.nextPage}>Next</button>
@@ -101,53 +99,53 @@ class WizardForm extends Component {
                 {page === 2 &&
                     <Grid className="pizza-container"> 
                         <h1>Form</h1>
-                            <div>
-                                <TextInput 
-                                    onChange={e => this.onInput(e)}
-                                    name="name"
-                                    value={name}
-                                    validate={val => !val ? 'Name is required' : ''} />
-                                <TextInput 
-                                    onChange={e => this.onInput(e)}
-                                    name="address"
-                                    value={address}
-                                    validate={val => !val ? 'Address is required' : ''} />
-                                <TextInput 
-                                    onChange={e => this.onInput(e)}
-                                    name="city"
-                                    value={city}
-                                    validate={val => !val ? 'City is required' : ''} />
-                                <TextInput 
-                                    onChange={e => this.onInput(e)}
-                                    name="telephone"
-                                    value={telephone}
-                                    validate={val => !validator.isMobilePhone(val, 'any') ? 'Telephone is required' : ''} />
-                                <TextInput 
-                                    onChange={e => this.onInput(e)}
-                                    name="postCode"
-                                    value={postCode}
-                                    validate={val => !validator.isPostalCode(val, 'IS')? 'PostCode is required' : ''} />
-                            </div>
-                            <div>
-                                <button type="button" className="previous" onClick={this.previousPage}>
+                        <div>
+                            <TextInput 
+                                onChange={e => this.onInput(e)}
+                                name="name"
+                                value={name}
+                                validate={val => !val ? 'Name is required' : ''} />
+                            <TextInput 
+                                onChange={e => this.onInput(e)}
+                                name="address"
+                                value={address}
+                                validate={val => !val ? 'Address is required' : ''} />
+                            <TextInput 
+                                onChange={e => this.onInput(e)}
+                                name="city"
+                                value={city}
+                                validate={val => !val ? 'City is required' : ''} />
+                            <TextInput 
+                                onChange={e => this.onInput(e)}
+                                name="telephone"
+                                value={telephone}
+                                validate={val => !validator.isMobilePhone(val, 'any') ? 'Telephone is required' : ''} />
+                            <TextInput 
+                                onChange={e => this.onInput(e)}
+                                name="postCode"
+                                value={postCode}
+                                validate={val => !validator.isPostalCode(val, 'IS')? 'PostCode is required' : ''} />
+                        </div>
+                        <div>
+                            <button type="button" className="previous" onClick={this.previousPage}>
                                     Previous
-                                </button>
-                                <button type="button" className="next" onClick={ (e) => this.onFormSubmit(e)} >Next</button>
-                            </div>
-            </Grid>}
-            {page === 3 &&
+                            </button>
+                            <button type="button" className="next" onClick={ (e) => this.onFormSubmit(e)} >Next</button>
+                        </div>
+                    </Grid>}
+                {page === 3 &&
                 <Grid>
-                <p> her serðu körfuna þina </p>
-                <button type="button" className="previous" onClick={this.previousPage}>Previous</button>
-                <button type="button" className="next" onClick={this.nextPage}>Confirme</button>
+                    <p> her serðu körfuna þina </p>
+                    <button type="button" className="previous" onClick={this.previousPage}>Previous</button>
+                    <button type="button" className="next" onClick={this.nextPage}>Confirme</button>
                 </Grid>
-            }
-            {page === 4 &&
+                }
+                {page === 4 &&
                 <Grid>
-                <p> Pöntun þin er komin í ofn!  </p>
+                    <p> Pöntun þin er komin í ofn!  </p>
                 </Grid>
-            }
-        </div>
+                }
+            </div>
         );
     }
 }
